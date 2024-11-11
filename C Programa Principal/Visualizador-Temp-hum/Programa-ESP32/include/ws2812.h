7701ab5906c17dfeb680b856517ac050b591c671
Nicolás Barrionuevo <nicolasbarrionuevofacultad@gmail.com>
2024-11-11 13:38:00 -0300

    Documentación y prueba de integración con flask

--- C Programa Principal/Visualizador-Temp-hum/Programa-ESP32/include/ws2812.h
+++ C Programa Principal/Visualizador-Temp-hum/Programa-ESP32/include/ws2812.h
@@ -3,14 +3,15 @@
 
 #include <Adafruit_NeoPixel.h>
 
+// Clase para controlar el LED WS2812
 class WS2812 {
   public:
-    WS2812(int pin, int numPixeles);
-    void begin();
-    void cambiarColor(uint8_t r, uint8_t g, uint8_t b);
+    WS2812(int pin, int numPixeles);  // Constructor
+    void begin();  // Inicializa el LED
+    void cambiarColor(uint8_t r, uint8_t g, uint8_t b);  // Cambia el color del LED
 
   private:
-    Adafruit_NeoPixel strip;
+    Adafruit_NeoPixel strip;  // Objeto para controlar el LED
 };
 
 #endif
